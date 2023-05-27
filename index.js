4905c44edc9ea4c61bb62e40308ea6ec33413305
Bogar Martinez <bogar.cm@icloud.com>
2023-05-26 20:16:02 -0500

    another one done

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+document.querySelector("main").remove();
+const newHeader = document.createElement("h1");
+newHeader.textContent = "Bogar is the champion";
+newHeader.id = "victory";
